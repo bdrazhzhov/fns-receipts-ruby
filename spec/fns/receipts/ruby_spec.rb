29a559f095d312c4a8b591e364371590c552c1d9
Boris Drazhzhov <templar8@gmail.com>
2023-10-25 23:33:44 +0300

    Initial commit

---
+++ spec/fns/receipts/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fns::Receipts::Ruby do
+  it "has a version number" do
+    expect(Fns::Receipts::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
